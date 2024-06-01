c2a85d3ee97f9caae8e0ce793d211f822b2a0be9
LLeoChan <leochan532780@gmail.com>
2024-06-02 02:27:18 +0800

    Deploy docs

---
+++ .umirc.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'My Component Library',
+  mode: 'site',
+  resolve: {
+    includes: ['docs'],
+  },
+};
---
+++ docx/index.md
@@ -0,0 +1,8 @@
+# My Component Library
+
+这是我的组件库文档。
+
+## 安装
+
+```bash
+npm install my-component-library
