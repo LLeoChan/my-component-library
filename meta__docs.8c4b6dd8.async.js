e756d054f63617e5e7e003874712ed63c177dd9a
LLeoChan <leochan532780@gmail.com>
2024-06-02 11:34:08 +0800

    Updates

---
+++ meta__docs.8c4b6dd8.async.js
@@ -0,0 +1,21 @@
+"use strict";(self.webpackChunkmy_component_library_leochan532=self.webpackChunkmy_component_library_leochan532||[]).push([[904],{32598:function(t,n,r){r.r(n),r.d(n,{texts:function(){return e}});const e=[{value:"\u5168\u5C40\u914D\u7F6E\u7EC4\u4EF6\u3002",paraId:0,tocIndex:0},{value:`<template>\r
+  <ConfigProvider :theme="theme">\r
+    <div>\u5185\u5BB9</div>\r
+  </ConfigProvider>\r
+</template>\r
+\r
+<script>\r
+import { ConfigProvider } from 'my-component-library';\r
+\r
+export default {\r
+  components: {\r
+    ConfigProvider,\r
+  },\r
+  data() {\r
+    return {\r
+      theme: 'light',\r
+    };\r
+  },\r
+};\r
+<\/script>
+`,paraId:1,tocIndex:1}]}}]);
